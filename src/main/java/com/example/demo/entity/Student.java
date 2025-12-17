e6b7ed9cb73962c0c4efc44ed07b63fc06cd2cd1
bala-amypo <bala@amypo.in>
2025-12-17 04:32:23 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -3,7 +3,7 @@ package com.example.demo.entity;
 import java.time.LocalDate;
 
 public class Student{
-    `private Long id;
+    private Long id;
     private String name;
     private String dept;
     private LocalDate dob;
